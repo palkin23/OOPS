8289c46b51e45ee3062bfcd86f67528ba78ddb52
palkin23 <palkingupts23@gmail.com>
2026-06-30 15:56:37 +0530

    Constructor overloading

---
+++ Constructor/constructoroverloading.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class Car {
+  public:
+    string brand;
+    string model;
+
+    Car() {
+      brand = "Unknown";
+      model = "Unknown";
+    }
+
+    Car(string b, string m) {
+      brand = b;
+      model = m;
+    }
+};
+
+int main() {
+  Car car1;
+  Car car2("BMW", "X5");
+  Car car3("Ford", "Mustang");
+  Car car4;
+
+  cout << "Car1: " << car1.brand << " " << car1.model << "\n";
+  cout << "Car2: " << car2.brand << " " << car2.model << "\n";
+  cout << "Car3: " << car3.brand << " " << car3.model<<"\n";
+  cout<<"Car4: "<<car4.brand<<" "<<car4.model;
+  return 0;
+}
