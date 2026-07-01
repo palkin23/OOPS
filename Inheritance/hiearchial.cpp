8beedd54a56cfda56b778639204dd045ab2d97a9
palkin23 <palkingupts23@gmail.com>
2026-07-01 14:21:43 +0530

    Hiearchial Inheritance

---
+++ Inheritance/hiearchial.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    void eat() {
+        cout << "Eating\n";
+    }
+};
+
+class Dog : public Animal {
+public:
+    void bark() {
+        cout << "Barking\n";
+    }
+};
+
+class Cat : public Animal {
+public:
+    void meow() {
+        cout << "Meowing\n";
+    }
+};
+
+int main() {
+
+    Dog d;
+    d.eat();
+    d.bark();
+
+    Cat c;
+    c.eat();
+    c.meow();
+
+    return 0;
+}
